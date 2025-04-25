727e0af4b1ca48be17acb46a33d3f4b6a29d4a4a
Paolo <159697381+paolomalgarin@users.noreply.github.com>
2025-04-25 15:41:18 +0200

    Add files via upload
    Signed-off-by: Paolo <159697381+paolomalgarin@users.noreply.github.com>

---
+++ DigitML - PHPOnly/NeuralNetwork/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'paolo/test_nn',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ankane/onnxruntime' => array(
+            'pretty_version' => 'v0.2.6',
+            'version' => '0.2.6.0',
+            'reference' => '4dadec73962d01c86ef3dc1cb552288941ce85af',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ankane/onnxruntime',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'paolo/test_nn' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
