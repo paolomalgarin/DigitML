a9c90c2035d0af844f8278eef0eafdf907e40972
Paolo <159697381+paolomalgarin@users.noreply.github.com>
2025-04-25 15:42:31 +0200

    Add files via upload
    Signed-off-by: Paolo <159697381+paolomalgarin@users.noreply.github.com>

---
+++ DigitML - PHPOnly/NeuralNetwork/vendor/ankane/onnxruntime/src/Vendor.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace OnnxRuntime;
+
+class Vendor
+{
+    public const VERSION = '1.21.0';
+
+    public const PLATFORMS = [
+        'x86_64-linux' => [
+            'file' => 'onnxruntime-linux-x64-{{version}}',
+            'checksum' => '7485c7e7aac6501b27e353dcbe068e45c61ab51fbaf598d13970dfae669d20bf',
+            'lib' => 'libonnxruntime.so.{{version}}',
+            'ext' => 'tgz'
+        ],
+        'aarch64-linux' => [
+            'file' => 'onnxruntime-linux-aarch64-{{version}}',
+            'checksum' => '4508084bde1232ee1ab4b6fad2155be0ea2ccab1c1aae9910ddb3fb68a60805e',
+            'lib' => 'libonnxruntime.so.{{version}}',
+            'ext' => 'tgz'
+        ],
+        'x86_64-darwin' => [
+            'file' => 'onnxruntime-osx-x86_64-{{version}}',
+            'checksum' => '8305afd2d75ee5702844a23b099d41885af30ad3d1b4cf3d8d795e3d8c1f9396',
+            'lib' => 'libonnxruntime.{{version}}.dylib',
+            'ext' => 'tgz'
+        ],
+        'arm64-darwin' => [
+            'file' => 'onnxruntime-osx-arm64-{{version}}',
+            'checksum' => '5c3f2064ee97eb7774e87f396735c8eada7287734f1bb7847467ad30d4036115',
+            'lib' => 'libonnxruntime.{{version}}.dylib',
+            'ext' => 'tgz'
+        ],
+        'x64-windows' => [
+            'file' => 'onnxruntime-win-x64-{{version}}',
+            'checksum' => '5c07bb2805cd666dda75fa9bfa60e75f2f90d478b952298dd9d55c00740d81bf',
+            'lib' => 'onnxruntime.dll',
+            'ext' => 'zip'
+        ]
+    ];
+
+    public static function check($event = null)
+    {
+        $dest = self::defaultLib();
+        if (file_exists($dest)) {
+            echo "✔ ONNX Runtime found\n";
+            return;
+        }
+
+        $dir = self::libDir();
+        if (!file_exists($dir)) {
+            mkdir($dir);
+        }
+
+        echo "Downloading ONNX Runtime...\n";
+
+        $file = self::platform('file');
+        $ext = self::platform('ext');
+        $url = self::withVersion("https://github.com/microsoft/onnxruntime/releases/download/v{{version}}/$file.$ext");
+        $contents = file_get_contents($url);
+
+        $checksum = hash('sha256', $contents);
+        if ($checksum != self::platform('checksum')) {
+            throw new Exception("Bad checksum: $checksum");
+        }
+
+        $tempDest = tempnam(sys_get_temp_dir(), 'onnxruntime') . '.' . $ext;
+        file_put_contents($tempDest, $contents);
+
+        $archive = new \PharData($tempDest);
+        if ($ext != 'zip') {
+            $archive = $archive->decompress();
+        }
+        $archive->extractTo(self::libDir(), overwrite: true);
+
+        echo "✔ Success\n";
+    }
+
+    public static function defaultLib()
+    {
+        return self::libDir() . '/' . self::libFile();
+    }
+
+    private static function libDir()
+    {
+        return __DIR__ . '/../lib';
+    }
+
+    private static function libFile()
+    {
+        return self::withVersion(self::platform('file') . '/lib/' . self::platform('lib'));
+    }
+
+    private static function platform($key)
+    {
+        return self::PLATFORMS[self::platformKey()][$key];
+    }
+
+    private static function platformKey()
+    {
+        if (PHP_OS_FAMILY == 'Windows') {
+            return 'x64-windows';
+        } elseif (PHP_OS_FAMILY == 'Darwin') {
+            if (php_uname('m') == 'x86_64') {
+                return 'x86_64-darwin';
+            } else {
+                return 'arm64-darwin';
+            }
+        } else {
+            if (php_uname('m') == 'x86_64') {
+                return 'x86_64-linux';
+            } else {
+                return 'aarch64-linux';
+            }
+        }
+    }
+
+    private static function withVersion($str)
+    {
+        return str_replace('{{version}}', self::VERSION, $str);
+    }
+}
